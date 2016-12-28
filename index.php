1b59c1ec2480ba942dd3f2b124a1294850dbcd05
lei.zhang <lei.zhang@qyer.com>
2016-12-28 14:20:52 +0800

    for zhifang

--- index.php
+++ index.php
@@ -1,1 +1,98 @@
-这是一个脂肪的网站
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>某出版社</title>
+<link href="http://common1.qyerstatic.com/place/common/basic/css/base.css" rel="stylesheet" type="text/css" />
+<link href="http://common1.qyerstatic.com/place/project/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="plcContainer">
+    <div class="qyWrap">
+        <h2 class="plcHotPlaceTitle bigTitle fontYaHei">某出版社热门图书</h2>
+        <div class="plcHotPlaceContent">
+            <ul class="plcHotPlaceList clearfix">
+                <li>
+                    <a href="#">
+                        <p class="top">
+                            <img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" data-original="http://common2.qyerstatic.com/place/project/images/tmp/poi.jpg" alt="" class="photo lazy" />
+                        </p>
+                        <div class="bottom">
+                            <h3 class="title fontYaHei">杜文传</h3>
+                            <div class="info">
+                                这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p class="top">
+                            <img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" data-original="http://common2.qyerstatic.com/place/project/images/tmp/poi.jpg" alt="" class="photo lazy" />
+                        </p>
+                        <div class="bottom">
+                            <h3 class="title fontYaHei">杜文传</h3>
+                            <div class="info">
+                                这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p class="top">
+                            <img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" data-original="http://common2.qyerstatic.com/place/project/images/tmp/poi.jpg" alt="" class="photo lazy" />
+                        </p>
+                        <div class="bottom">
+                            <h3 class="title fontYaHei">杜文传</h3>
+                            <div class="info">
+                                这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p class="top">
+                            <img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" data-original="http://common2.qyerstatic.com/place/project/images/tmp/poi.jpg" alt="" class="photo lazy" />
+                        </p>
+                        <div class="bottom">
+                            <h3 class="title fontYaHei">杜文传</h3>
+                            <div class="info">
+                                这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p class="top">
+                            <img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" data-original="http://common2.qyerstatic.com/place/project/images/tmp/poi.jpg" alt="" class="photo lazy" />
+                        </p>
+                        <div class="bottom">
+                            <h3 class="title fontYaHei">杜文传</h3>
+                            <div class="info">
+                                这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p class="top">
+                            <img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" data-original="http://common2.qyerstatic.com/place/project/images/tmp/poi.jpg" alt="" class="photo lazy" />
+                        </p>
+                        <div class="bottom">
+                            <h3 class="title fontYaHei">杜文传</h3>
+                            <div class="info">
+                                这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书这是一本介绍杜文的图书
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
