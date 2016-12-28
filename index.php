b22982fc422a0ea9505bdf51a8e2526fd517ff2e
zhanglei <lei.zhang@qyer.com>
2016-12-28 14:36:55 +0800

    for zhifang

--- index.php
+++ index.php
@@ -3,6 +3,32 @@
 <link href='./static/css/base.css' rel='stylesheet' type='text/css' />
 <link href='./static/css/style.css' rel='stylesheet' type='text/css' />
 
+<div class="plcFirstModule">
+	<div class="plcFirstModuleWrap">
+
+		<!-- 左侧焦点图 before -->
+		<div class="plcFocusPhoto" id="focusPhoto">
+			<ul class="plcFocusPhotoList">
+				<li><a href="#"><img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" width="680" height="400" alt="" /></a></li>
+				<li><a href="#"><img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" width="680" height="400" alt="" /></a></li>
+				<li><a href="#"><img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" width="680" height="400" alt="" /></a></li>
+				<li><a href="#"><img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" width="680" height="400" alt="" /></a></li>
+				<li><a href="#"><img src="http://common2.qyerstatic.com/place/project/images/tmp/focusphoto.jpg" width="680" height="400" alt="" /></a></li>
+			</ul>
+			<p class="prev" title="上一张"></p>
+			<p class="next" title="下一张"></p>
+			<p class="photoMore"><a href="#">73895张照片</a></p>
+			<p class="photoAdmin"><a href="javascript:void(0);">管理封图</a></p>
+		</div>
+		<!-- 左侧焦点图 end -->
+
+		<div class="plcFocusOperate">
+            这边是着重要强烈推荐的书籍这边是着重要强烈推荐的书籍这边是着重要强烈推荐的书籍这边是着重要强烈推荐的书籍这边是着重要强烈推荐的书籍
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+
 <div class="plcContainer">
     <div class="qyWrap">
         <h2 class="plcHotPlaceTitle bigTitle fontYaHei">某出版社热门图书</h2>
